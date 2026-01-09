a9b6d6fa09aae5c99ce698dcbee0c7c7d3589971
geordanny <geordanipinales@gmail.com>
2026-01-08 19:48:26 -0500

    remove pages, and images. add details style a little bit the index page

--- src/_data/meta.js
+++ src/_data/meta.js
@@ -1,24 +1,24 @@
 export const url = process.env.URL || 'http://localhost:8080';
 // Extract domain from `url`
 export const domain = new URL(url).hostname;
-export const siteName = 'Eleventy Excellent';
-export const siteDescription = 'Eleventy starter for building modern, resilient websites';
+export const siteName = 'Geordanny\'s notebook';
+export const siteDescription = 'The website of someone who likes computers';
 export const siteType = 'Person'; // schema
 export const locale = 'en_EN';
 export const lang = 'en';
 export const skipContent = 'Skip to content';
 export const author = {
-  name: 'Lene Saile', // i.e. Lene Saile - page / blog author's name. Must be set.
+  name: 'Geordanny', // i.e. Lene Saile - page / blog author's name. Must be set.
   avatar: '/icon-512x512.png', // path to the author's avatar. In this case just using a favicon.
-  email: 'hola@lenesaile.com', // i.e. hola@lenesaile.com - email of the author
-  website: 'https://www.lenesaile.com', // i.e. https.://www.lenesaile.com - the personal site of the author
-  fediverse: '@lene@front-end.social' // used for highlighting journalism on the fediverse. Can be Mastodon, Flipboard, Threads, WordPress (with the ActivityPub plugin installed), PeerTube, Pixelfed, etc. https://blog.joinmastodon.org/2024/07/highlighting-journalism-on-mastodon/
+  email: 'geordanipinales@gmail.com', // i.e. hola@lenesaile.com - email of the author
+  website: 'https://www.geordanny.me', // i.e. https.://www.lenesaile.com - the personal site of the author
+  fediverse: '@geordanny@mastodon.social'
 };
 export const creator = {
-  name: 'Lene Saile', // i.e. Lene Saile - creator's (developer) name.
-  email: 'hola@lenesaile.com',
-  website: 'https://www.lenesaile.com',
-  social: 'https://front-end.social/@lene'
+  name: 'Geordanny', // i.e. Lene Saile - creator's (developer) name.
+  website: 'https://www.geordanny.me',
+  email: 'geordanipinales@gmail.com',
+  social: 'https://mastodon.social/@geordanny'
 };
 export const pathToSvgLogo = 'src/assets/svg/misc/logo.svg'; // used for favicon generation
 export const themeColor = '#dd4462'; // used in manifest, for example primary color value
@@ -26,11 +26,11 @@ export const themeLight = '#f8f8f8'; // used for meta tag theme-color, if light 
 export const themeDark = '#2e2e2e'; // used for meta tag theme-color, if dark colors are prefered. best use value set for dark bg
 export const opengraph_default = '/assets/images/template/opengraph-default.jpg'; // fallback/default meta image
 export const opengraph_default_alt =
-  "Visible content: An Eleventy starter with CUBE CSS, Cube CSS, Every Layout, Design Tokens and Tailwind for uitility classes. A workflow for building modern and resilient websites, introduced by Andy Bell's project buildexcellentwebsit.es"; // alt text for default meta image"
+  "Geordanny's Notebook - A personal blog about tech and college experiences";
 export const blog = {
+  name: 'My personal blog',
   // RSS feed
-  name: 'My Web Development Blog',
-  description: 'Tell the word what you are writing about in your blog. It will show up on feed readers.',
+description: 'I will share about my tech journey and college experiences',
   // feed links are looped over in the head. You may add more to the array.
   feedLinks: [
     {
